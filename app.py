52914ce58444f96d8acd6baaf1f041cafdfdbb91
Nirmala Koshiga <101269369+NIRMALAKOSHIGA@users.noreply.github.com>
2023-02-28 14:45:19 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-
-st.title("Hello Guys :wave: Im :red[Nirmala Koshiga]")
-st.header("Data Science Intenship@Innomatics Reasech Labs")
-st.subheader("I am pursuing PGDM final year with specialigation of Artificial Inttelegence and Machine Learning.I have done an Intenship at Choice Solution Limited as Database Developer.I have good knowleged on Machine Learning , MSql, PowerBI, Tableau, R-Language. ")
-st.subheader("You can cacht me on:sunglasses:https://www.linkedin.com/in/nirmala-koshiga/")
-
-
