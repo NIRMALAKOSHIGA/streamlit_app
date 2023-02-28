000d4a6c2a499fd76fa5c7f0e2177a35b4faa5dc
Nirmala Koshiga <101269369+NIRMALAKOSHIGA@users.noreply.github.com>
2023-02-28 13:11:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import streamlit as st
+from matplotlib import image
+import pandas as pd
+import plotly.express as px
+
+st.title("Hello Guys :wave: Im :red[Nirmala Koshiga]")
+st.header("Data Science Intenship@Innomatics Reasech Labs")
+st.subheader("I am pursuing PGDM final year with specialigation of Artificial Inttelegence and Machine Learning.I have done an Intenship at Choice Solution Limited as Database Developer.I have good knowleged on Machine Learning , MSql, PowerBI, Tableau, R-Language. ")
+st.subheader("You can cacht me on:sunglasses:https://www.linkedin.com/in/nirmala-koshiga/")
+
+
